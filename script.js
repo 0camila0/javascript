468cd238a1dc9e49876f1fb7cc31adc90df074e3
0camila0 <111430783+0camila0@users.noreply.github.com>
2024-08-14 11:27:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+funciton carregar(){ 
+var msg = window.document.getElementById('msg')
+  var img = aindow.document.get.ElementById('imagem')
+  var data = New Date() //pega a hora da data atual
+  var hora= data.getHours()//pega a hora da data atual
+      msg.InnerHTML = `Agora são ${hora} horas.`
+      if (hora >=0 && hora < 12){
+        //Bom dia 
+        img.scr = 'manha.jpeg'
+      } else if(hora >=12 && hora <18){
+             //Boa tarde
+             img.scr = 'tarde.jpeg'
+        } else {
+             //Boa noite 
+             img.scr = 'noite.jpeg'
+      }
